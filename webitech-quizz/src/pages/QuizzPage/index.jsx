c689149d959d37217d10d74ad62b497aadee6a06
JulienPoirierWebDev <connexion@julienpoirier-Webdev.com>
2024-01-02 14:18:03 +0100

    première solution pour compter les points et faire un feedback : solution final quizz

--- webitech-quizz/src/pages/QuizzPage/index.jsx
+++ webitech-quizz/src/pages/QuizzPage/index.jsx
@@ -5,6 +5,8 @@ const QuizzPage = () => {
   //depuis l'API -> useEffect
   const [questions, setQuestions] = useState([]);
   const [questionIndex, setQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
 
   useEffect(() => {
     const controller = new AbortController();
@@ -35,6 +37,13 @@ const QuizzPage = () => {
   // Afficher les réponses
 
   // Permettre de répondre à la question
+
+  const answers = questions[questionIndex]?.incorrect_answers || [];
+  answers.push(questions[questionIndex]?.correct_answer);
+
+  console.log(answers);
+  console.log(answers.sort(() => Math.random() - 0.5));
+
   return (
     <>
       QuizzPage
@@ -51,20 +60,64 @@ const QuizzPage = () => {
           </button>
         </>
       )} */}
-      {questions.length > 0 ? (
+      {questions.length > 0 && questionIndex < 10 ? (
         <>
-          <div>{questions[questionIndex]?.question}</div>
+          <div>
+            <p
+              dangerouslySetInnerHTML={{
+                __html: questions[questionIndex].question,
+              }}
+            ></p>
+          </div>
 
-          <button
-            onClick={() => {
-              setQuestionIndex(questionIndex + 1);
-            }}
-          >
-            Question suivante
-          </button>
+          <div>
+            {answers.map((answer) => (
+              <p
+                key={answer}
+                onClick={() => {
+                  console.log(answer);
+                  console.log(
+                    answer === questions[questionIndex].correct_answer
+                  );
+                  if (answer === questions[questionIndex].correct_answer) {
+                    setScore(score + 1);
+                  }
+                  setUserAnswers([...userAnswers, answer]);
+                  setQuestionIndex(questionIndex + 1);
+                }}
+              >
+                {answer}
+              </p>
+            ))}
+          </div>
         </>
       ) : (
         <div>Pas de questions</div>
+      )}
+      {questionIndex === 10 && (
+        <div>
+          <p>Score : {score}</p>
+          {questions.map((question, index) => (
+            <div key={question.question}>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: question.question,
+                }}
+              ></p>
+              {userAnswers[index] === question.correct_answer ? (
+                <p>
+                  Tu as trouvé la bonne réponse ! C&rsquo;était{" "}
+                  {question.correct_answer}
+                </p>
+              ) : (
+                <p>
+                  Tu t&rsquo;es trompé, la bonne réponse était :{" "}
+                  {question.correct_answer}. Et tu as mis : {userAnswers[index]}
+                </p>
+              )}
+            </div>
+          ))}
+        </div>
       )}
     </>
   );
